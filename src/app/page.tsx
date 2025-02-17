45f595680b1b9dd144f94acfffead41081dd9076
srt-1909 <sachinthakkar1992002@gmail.com>
2025-02-17 14:48:54 +0530

    pricacy policy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-    Hello
-    </>
-    
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      {/* <h1 className="text-4xl font-semibold text-blue-600 mb-4">Hello</h1> */}
+      <p className="text-lg text-gray-700">
+        To learn more about our privacy policy, click{" "}
+        <Link
+          href="/privacy-policy"
+          className="text-blue-500 hover:text-blue-700 font-semibold"
+        >
+          here
+        </Link>
+        .
+      </p>
+    </div>
   );
 }
